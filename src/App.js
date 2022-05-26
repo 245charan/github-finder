1fce7a663d07ce4c134a7cd79d6b742597af2229
charan <charan.netha@acsicorp.com>
2022-05-27 00:58:02 +0530

    useReducer got introduced

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-
+import {GithubProvider} from './components/context/github/GithubContext.jsx'
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/components/context/github/GithubContext.jsx
+++ src/components/context/github/GithubContext.jsx
@@ -1,6 +1,51 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const REACT_APP_GITHUB_URL = {process.env.REACT_APP_GITHUB_URL}
-const REACT_APP_GITHUB_TOKEN = {process.env.REACT_APP_GITHUB_TOKEN}
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const fetchUsers = async () => {
+    setLoading();
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+
+    console.log(data);
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+
+  const setLoading = () =>
+    dispatch({
+      type: "SET_LOADING",
+    });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
