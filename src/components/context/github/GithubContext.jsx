e69a4dce52b77ca5af76b55dd79c0f9db459e44e
charan <charan.netha@acsicorp.com>
2022-05-27 22:07:32 +0530

    Get search results, from search bar

--- src/components/context/github/GithubContext.jsx
+++ src/components/context/github/GithubContext.jsx
@@ -14,22 +14,50 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //  Get initial users(testing purposes)
-  const fetchUsers = async () => {
+  //  Get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
+    const { items } = await response.json();
 
-    console.log(data);
+    console.log(items);
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
+
+  //   Clear users from state
+  const clearUsers = () => {
+    dispatch({
+      type: "CLEAR_USERS",
+    });
+  };
+
+  //  Get initial users(testing purposes)
+  //   const fetchUsers = async () => {
+  //     setLoading();
+  //     const response = await fetch(`${GITHUB_URL}/users`, {
+  //       headers: {
+  //         Authorization: `token ${GITHUB_TOKEN}`,
+  //       },
+  //     });
+  //     const data = await response.json();
+
+  //     console.log(data);
+  //     dispatch({
+  //       type: "GET_USERS",
+  //       payload: data,
+  //     });
+  //   };
 
   const setLoading = () =>
     dispatch({
@@ -41,7 +69,8 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
       }}
     >
       {children}
